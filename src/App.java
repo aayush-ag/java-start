c0b99ca9252beb04a6b66dd1ebf40fb752e3546a
Aayush <aayush23467@gmail.com>
2023-07-10 10:20:45 +0000

    git-ignore

--- src/App.java
+++ src/App.java
@@ -3,13 +3,24 @@ public class App {
         System.out.println("Hello, World!");
     }
     public static void main(String[] args) throws Exception {
-        int x = 20;
-        int y = 18;
-        if(y > x){
-            System.out.println("y is higher");
+        int marks = 85;
+        if(marks < 50){
+            System.out.println("fail");
+        }
+        else if (marks >= 50 && marks < 60){
+            System.out.println("D grade");
+        }
+        else if (marks >= 60 && marks < 75){
+            System.out.println("C grade");
+        }
+        else if (marks >= 75 && marks < 90){
+            System.out.println("B grade");
+        }
+        else if (marks >= 90 && marks <= 100){
+            System.out.println("A+ grade");
         }
         else{
-            System.out.println("x is greater");
+            System.out.println("Invalid");
         }
     }
 }
