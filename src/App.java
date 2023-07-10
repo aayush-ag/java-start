e913b88996d638f0328731ba24fcd2a5c610b1cc
Aayush <aayush23467@gmail.com>
2023-07-10 17:23:03 +0000

    pattern

--- src/App.java
+++ src/App.java
@@ -73,7 +73,17 @@ public class App {
             System.out.println(i);
         }
     }
+    
+    public static void pattern() throws Exception {
+        int n =5;
+        for(int i=0; i<n; i++){
+            for (int j=0; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) throws Exception {
-        forloop2();
+        pattern();
     }
 }
