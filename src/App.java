e4ab3907715d0a8a4bf5c0a81449e309253aa715
Aayush <aayush23467@gmail.com>
2023-07-10 10:10:40 +0000

    if-else

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void Hello(String[] args) throws Exception {
         System.out.println("Hello, World!");
+    }
+    public static void main(String[] args) throws Exception {
+        int x = 20;
+        int y = 18;
+        if(y > x){
+            System.out.println("y is higher");
+        }
+        else{
+            System.out.println("x is greater");
+        }
     }
 }
